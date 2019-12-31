0392c41a479d492ca68c86db051d551ff41cd4d9
Eddie Hernandez <mchh.004@gmail.com>
2019-12-31 00:13:20 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,32 +32,24 @@
 
 
 def naming_system
-  family = {
- :label => "Family",
- :sub_category => {
-   :label => "Genus",
-   :sub_category => {
-       :label => "Species",
- :sub_category => nil
-   }
- }
+  hash = {
+    label: "Kingdom",
+      sub_category: {
+          label: "Phylum",
+          sub_category: {
+            label: "Class"
+    }
+  }
+    label: "Order",
+    label: "Family",
+      sub_category: {
+       label: "Genus",
+    }
+    label: "Species",
+      sub_category: nil
 }
-  order = {
-  :label => "Order",
-  :sub_category => family
-}
-   domain = {
- :label => "Kingdom",
-  :sub_category => {
-    :label => "Phylum",
-    :sub_category => {
-      :label => "Class",
-       :sub_category => order
-   }
- }
-}
- domain
-end
+hash
+
 
   # Remember:
   #  Kingdom
