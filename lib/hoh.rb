d0630b2d6a901070999fe8cb82b1aaa2dff180a7
Eddie Hernandez <mchh.004@gmail.com>
2019-12-31 00:48:30 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -33,21 +33,14 @@
 
 def naming_system
   hoh = {
-    label: "Kingdom", sub_category: {
-      label: "Phylum", sub_category: {
-        label: "Class", sub_category: {
-          label: "Order", sub_category:{
-            label: "Family", sub_category: {
-              label: "Genus", sub_category: {
-                label: "Species", sub_category: {
-                  nil
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+    label: "Kingdom",
+    sub_category: { label: "Phylum",
+    sub_category: { label: "Class",
+    sub_category: { label: "Order",
+    sub_category: { label: "Family",
+    sub_category: { label: "Genus",
+    sub_category: { label: "Species",
+    sub_category: { nil } } } } } } }
   }
 
   # Remember:
