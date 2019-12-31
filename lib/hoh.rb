d68ed2ce9bdb653198dca25993bcdff477c9a293
Eddie Hernandez <mchh.004@gmail.com>
2019-12-31 00:17:39 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -34,7 +34,7 @@
 def naming_system
   hash = {
     label: "Kingdom",
-    sub_category: { label: "Phylum", sub_category: { label: "Class" }
+    sub_category: { label: "Phylum", sub_category: { label: "Class" } }
     label: "Order",
     label: "Family", sub_category: { label: "Genus" }
     label: "Species",
