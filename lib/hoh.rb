6ed5b32cec616430398a57352a37a905a5831100
Eddie Hernandez <mchh.004@gmail.com>
2019-12-31 00:55:19 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -40,7 +40,14 @@ def naming_system
     sub_category: { label: "Family",
     sub_category: { label: "Genus",
     sub_category: { label: "Species",
-    sub_category: { nil } } } } } } }
+    sub_category: { nil
+      }
+      }
+      }
+       }
+        }
+         }
+        }
   }
   return naming_system
   # Remember:
