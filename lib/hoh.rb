44cb7888304b69c9b0d4aafd446c5f3fe3e580f5
Eddie Hernandez <mchh.004@gmail.com>
2019-12-31 00:04:09 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -33,12 +33,33 @@ PORTION_4 = {
 
 def naming_system
   HoH = {
-    PORTION_1,
-    PORTION_2,
-    PORTION_3,
-    PORTION_4
-  }
+    PORTION_1 = {
+      label: "Kingdom",
+      sub_category: {
+        label: "Phylum",
+        sub_category: {
+          label: "Class"
+        }
+      }
+    }
 
+    PORTION_2 = {
+      label: "Order"
+    }
+
+    PORTION_3 = {
+      label: "Family",
+      sub_category: {
+        label: "Genus",
+      }
+    }
+
+    PORTION_4 = {
+      label: "Species",
+      sub_category: nil
+    }
+  }
+  HoH
 
   # Remember:
   #  Kingdom
